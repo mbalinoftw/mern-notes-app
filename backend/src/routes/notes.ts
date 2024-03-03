b335001ce559ab00dc77ae3ab51fe52e43fecb3b
Mariano Baliño <marianobalinho@gmail.com>
2024-03-03 14:42:58 -0300

    add update && delete note controller/routes

--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { createNote, getNote, getNotes } from "../controllers/notes";
+import { createNote, deleteNote, getNote, getNotes, updateNote } from "../controllers/notes";
 
 const router = express.Router();
 
 router.get("/", getNotes);
 router.get("/:id", getNote);
 router.post("/", createNote);
+router.patch("/:id", updateNote);
+router.delete("/:id", deleteNote);
 
 export default router;
