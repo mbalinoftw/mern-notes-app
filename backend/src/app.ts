f78f0a1574e7648d4a763b37c5a36089abfd50ca
Mariano Baliño <marianobalinho@gmail.com>
2024-03-20 01:26:43 -0300

    add express session -> user auth && cookies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,8 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
+import session from "express-session";
+import env from "./utils/validateEnv";
+import MongoStore from "connect-mongo";
 import morgan from "morgan";
 import notesRoutes from "./routes/notes";
 import usersRoutes from "./routes/users";
@@ -11,6 +14,21 @@ const app = express();
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+    },
+    rolling: true,
+    store: MongoStore.create({
+      mongoUrl: env.MONGO_CONNECTION_STRING
+    })
+  })
+);
 
 app.use("/api/users", usersRoutes);
 app.use("/api/notes", notesRoutes);
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,8 +1,11 @@
 import express from "express";
-import { signUp } from "../controllers/users";
+import { getAuthenticatedUser, login, logout, signUp } from "../controllers/users";
 
 const router = express.Router();
 
+router.get("/", getAuthenticatedUser);
 router.post("/signup", signUp);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
